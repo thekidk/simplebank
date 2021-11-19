248a43086e475572bfc594df02c8c9e6e033951a
Kidane Mussie <kidanemussie@Kidanes-MacBook-Air.local>
2021-11-19 13:53:53 -0800

    Added unit tests

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,86 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/techschool/simplebank/util"
+)
+
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
+
+	helper1 := sql.NullInt64{Int64: account1.ID, Valid: true}
+	helper2 := sql.NullInt64{Int64: account2.ID, Valid: true}
+
+	arg := CreateTransferParams{
+		FromAccountID: helper1,
+		ToAccountID:   helper2,
+		Amount:        util.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	createRandomTransfer(t, account1, account2)
+}
+
+func TestGetTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer1 := createRandomTransfer(t, account1, account2)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+
+func TestListTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	for i := 0; i < 5; i++ {
+		createRandomTransfer(t, account1, account2)
+		createRandomTransfer(t, account2, account1)
+	}
+
+	helper1 := sql.NullInt64{Int64: account1.ID, Valid: true}
+
+	arg := ListTransfersParams{
+		FromAccountID: helper1,
+		ToAccountID:   helper1,
+		Limit:         5,
+		Offset:        5,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+		require.True(t, transfer.FromAccountID == helper1 || transfer.ToAccountID == helper1)
+	}
+}
